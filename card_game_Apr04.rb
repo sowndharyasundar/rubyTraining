718ce9d15f79f61d521279da557bdd0cab003620
sowndharyasundar <sowndharyasundar@gmail.com>
2017-04-12 14:16:01 +0530

    Grouping script by implementing modules
    Displaying suits, card names and card values in one module. And Filling deck and displaying deck in another module.

--- card_game_Apr04.rb
+++ card_game_Apr04.rb
@@ -1,54 +1,76 @@
+module Display_SuitsAndCards
+	def disp_suits(suits)
+		print "Card suits are: " +suits.join(" ")
+	end
+
+	def disp_card_per_suit(card_per_suit)
+		print "\n\nCard per suits: " +card_per_suit.to_s 
+	end
+
+	def disp_valueof_eachcard(card_value)
+		print"\n\nValue for each card: "+card_value.to_s+"\n"
+	end
+end
+
+module Fill_DisplayDeck
+	
+	def fill_deck
+		for @deck_index in 0..51
+			@deck.insert(@deck_index,CARD_PER_SUIT.at(@index_card_per_suit))
+			deck_element=@deck.at(@deck_index)
+			@deck_index+=1
+			@index_card_per_suit+=1
+			if(deck_element=="King")
+				@index_card_per_suit=0
+			end
+		end
+		# print "\nDeck contains:\n"+@deck.to_s
+	end
+
+	def display_cards_per_suit(card_suits)
+		@card_suits=card_suits
+		display_cards=@deck.each_slice(13).to_a
+		i=0
+		while(i<display_cards.size)
+			puts "\nDeck contains below cards in each suit: "
+			puts "\n"+@card_suits.at(i) +": "
+			print display_cards[i]
+			puts "\n" 
+			i+=1
+		end
+	end
+end
+
+
+class SuitsAndCards
+include Display_SuitsAndCards
+end
+
+class DeckAndPlayers
+
+include Fill_DisplayDeck
+	
+	def initialize
+		@deck=Array.new
+		@deck_index=0
+		@index_card_per_suit=0
+		@hand1=Array.new
+		@hand2=Array.new
+		@hand1_index=0
+		@hand2_index=0
+	end
+end
+
+
 card_suits=["Clubs","Diamonds","Hearts","Spades"]
 CARD_PER_SUIT=%w[Ace 2 3 4 5 6 7 8 9 10 Jack Queen King]
 card_value=Hash["Ace"=>11,2=>2,3=>3,4=>4,5=>5,6=>6,7=>7,8=>8,9=>9,"Jack"=>10,"Queen"=>10,"King"=>10]
-deck=Array.new
-hand1=Array.new
-hand2=Array.new
 
-index_card_per_suit=0
-deck_index=0
-for deck_index in 0..51
-		deck.insert(deck_index,CARD_PER_SUIT.at(index_card_per_suit))
-		deck_element=deck.at(deck_index)
-		deck_index+=1
-		index_card_per_suit+=1
-	if(deck_element=="King")
-		index_card_per_suit=0
-	end
-end
+card=SuitsAndCards.new
+card.disp_suits(card_suits)
+card.disp_card_per_suit(CARD_PER_SUIT)
+card.disp_valueof_eachcard(card_value)
 
-display_cards=deck.each_slice(13).to_a
-i=0
-	while(i<display_cards.size)
-		puts card_suits.at(i) +": "
-		print display_cards[i]
-		puts "\n" 
-		i+=1
-	end
-
-deck_index=0
-hand1_index=0
-hand2_index=0
-
-while(hand1_index<3)
-	hand1.insert(hand1_index,deck.at(deck_index))
-	hand1_index+=1
-	deck_index+=1
-end
-while(hand2_index<3)
-	hand2.insert(hand2_index,deck.at(deck_index))
-	hand2_index+=1
-	deck_index+=1
-end
-	hand1_index=0
-	hand2_index=0
-
-puts "\nHand one contains:\n" 
-for hand1_index in 0..hand1.size-1
- 	puts hand1.at(hand1_index) +" of " +card_suits.at(index_card_per_suit)
-end
-
-puts "\nHand two contains:\n" 
-for hand2_index in 0..hand2.size-1
- 	puts hand2.at(hand2_index) +" of " +card_suits.at(index_card_per_suit)
-end
+deckobj=DeckAndPlayers.new
+deckobj.fill_deck
+deckobj.display_cards_per_suit(card_suits)
